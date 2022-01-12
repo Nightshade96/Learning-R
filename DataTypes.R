cc1a336027e4e88a9c45b96e199f0796d0b6bbbf
Akshay Rajan <94909391+Nightshade96@users.noreply.github.com>
2022-01-12 12:13:19 -0500

    Add files via upload

---
+++ DataTypes.R
@@ -0,0 +1,23 @@
+#numeric/Decimals/Whole Numbers
+a <- 2.2
+
+#Logicals
+b <- TRUE
+
+c <- F
+
+d <- T
+
+#characters
+e <- "helloWorld"
+
+f <-  'Welcome'
+
+#checking data types
+
+class(a)
+class(b)
+class(c)
+class(d)
+class(e)
+class(f)
---
+++ Variables.R
@@ -0,0 +1,21 @@
+vari <- 100 #Assigining the value 100 to variable named vari
+
+vari
+
+bank <- 1000
+
+#ways of naming variables with multiple words
+
+#use a dot seperator
+bank.account <- 100 
+#Using camel casing
+bankAccount <- 100
+#use an underscore seperator is Bad Style in R
+bank_account <- 100
+
+
+deposit <- 10
+bank.account <- bank.account + deposit
+
+bank.account
+
